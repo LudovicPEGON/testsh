1647555178ac74b82e05255d3e64b9b79d393b55
lpegon <l.pegon@everteam.com>
2017-02-09 15:36:27 +0000

    Generated code by SwaggerHub

---
+++ jaxrs/src/main/java/io/swagger/api/factories/StoreApiServiceFactory.java
@@ -0,0 +1,13 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.StoreApiService;
+import io.swagger.api.impl.StoreApiServiceImpl;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-02-09T15:36:26.983Z")
+public class StoreApiServiceFactory {
+    private final static StoreApiService service = new StoreApiServiceImpl();
+
+    public static StoreApiService getStoreApi() {
+        return service;
+    }
+}
---
+++ jaxrs/src/main/java/io/swagger/api/factories/UserApiServiceFactory.java
@@ -0,0 +1,13 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.UserApiService;
+import io.swagger.api.impl.UserApiServiceImpl;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-02-09T15:36:26.983Z")
+public class UserApiServiceFactory {
+    private final static UserApiService service = new UserApiServiceImpl();
+
+    public static UserApiService getUserApi() {
+        return service;
+    }
+}
